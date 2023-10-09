ecab3ca4c1388dbff372d003be1f0eb31ca07f79
zguen <guillaume.mousin@gmail.com>
2023-10-09 19:11:52 +0200

    create generation et master ok

--- src/master/dto/create-master.dto.ts
+++ src/master/dto/create-master.dto.ts
@@ -1,1 +1,34 @@
-export class CreateMasterDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsEmail, IsNotEmpty, IsString, Matches } from "class-validator";
+
+export class CreateMasterDto {
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    lastName: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    firstName: string;
+
+    @ApiProperty()
+    @IsString()
+    nickName: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsEmail()
+    @Matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+    email: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$/)
+    password: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsBoolean()
+    admin: boolean;
+}
